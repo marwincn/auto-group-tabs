d40624fa2036887c6d1aa689baf74820d40ffd6e
marwincn <qiangbo.xu@gmail.com>
2021-10-10 14:32:32 +0800

    [Feature]支持按二级域名分组策略

--- src/popup/ConfigPage.js
+++ src/popup/ConfigPage.js
@@ -1,11 +1,21 @@
-import { Button, Divider, Form, Input, InputNumber, Radio, Switch } from "antd";
+import {
+  Button,
+  Divider,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Switch,
+  Alert,
+} from "antd";
 import "antd/dist/antd.css";
 import React from "react";
 import "./ConfigPags.css";
 
 const groupStrategyOptions = [
-  { label: 'Domain', value: 1 },
-  { label: 'Tab Title', value: 2 },
+  { label: "Domain", value: 1 },
+  { label: "SLD", value: 2 },
+  { label: "Tab Title", value: 3 },
 ];
 
 class ConfigPage extends React.Component {
@@ -21,34 +31,34 @@ class ConfigPage extends React.Component {
   }
 
   componentDidMount() {
-    return chrome.storage.sync.get(Object.keys(this.state), config => {
+    return chrome.storage.sync.get(Object.keys(this.state), (config) => {
       this.setState(config);
     });
   }
 
   onManuallyUpdateClick = () => {
-    chrome.runtime.sendMessage({groupRightNow: true});
+    chrome.runtime.sendMessage({ groupRightNow: true });
   };
 
-  onEnableAutoGroupChange = value => {
+  onEnableAutoGroupChange = (value) => {
     const newState = { enableAutoGroup: value };
     this.setState(newState);
     chrome.storage.sync.set(newState);
   };
 
-  onGroupTabNumChange = value => {
+  onGroupTabNumChange = (value) => {
     const newState = { groupTabNum: value };
     this.setState(newState);
     chrome.storage.sync.set(newState);
   };
 
-  onGroupStrategyChange = e => {
+  onGroupStrategyChange = (e) => {
     const newState = { groupStrategy: e.target.value };
     this.setState(newState);
     chrome.storage.sync.set(newState);
   };
 
-  onTabTitlePatternApply = value => {
+  onTabTitlePatternApply = (value) => {
     console.log(value);
     this.setState({ applyLoading: true });
     chrome.storage.sync.set({ tabTitlePattern: value }, () => {
@@ -56,33 +66,38 @@ class ConfigPage extends React.Component {
         this.setState({ applyLoading: false });
       }, 500);
     });
-  }
+  };
 
-  onTabTitlePatternChange = e => {
+  onTabTitlePatternChange = (e) => {
     this.setState({ tabTitlePattern: e.target.value });
-  }
+  };
 
   render() {
     return (
-      <div style={{ width: "300px", height: "420px", overflow: "auto" }}>
-        <Form
-          // labelCol={{ span: 10 }}
-          // wrapperCol={{ span: 14 }}
-          labelAlign="left"
-          layout="vertical"
-          style={{ padding: "16px" }}
-        >
-          <Form.Item style={{textAlign: "center"}}>
-            <Button type="primary" shape="round"  onClick={this.onManuallyUpdateClick}>
-              👏  Group all tabs right now!
+      <div style={{ width: "280px", height: "400px", overflow: "auto" }}>
+        <Form labelAlign="left" layout="vertical" style={{ padding: "16px" }}>
+          <Form.Item style={{ textAlign: "center" }}>
+            <Button
+              type="primary"
+              shape="round"
+              onClick={this.onManuallyUpdateClick}
+            >
+              👏 Group all tabs right now!
             </Button>
           </Form.Item>
-          <Divider />
+          <Divider style={{ margin: "12px 0" }} />
           <Form.Item label="Enable auto group tabs">
-            <Switch checked={this.state.enableAutoGroup} onChange={this.onEnableAutoGroupChange} />
+            <Switch
+              checked={this.state.enableAutoGroup}
+              onChange={this.onEnableAutoGroupChange}
+            />
           </Form.Item>
           <Form.Item label="Min-number of tabs per group">
-            <InputNumber min={1} value={this.state.groupTabNum} onChange={this.onGroupTabNumChange} />
+            <InputNumber
+              min={1}
+              value={this.state.groupTabNum}
+              onChange={this.onGroupTabNumChange}
+            />
           </Form.Item>
           <Form.Item label="Group strategy">
             <Radio.Group
@@ -92,18 +107,31 @@ class ConfigPage extends React.Component {
               optionType="button"
             />
           </Form.Item>
-          <Form.Item label="Tab title contains">
-            <Input.Search
-              style={{ width: '250px' }}
-              enterButton="Apply"
-              placeholder="Empty matches nothing"
-              value={this.state.tabTitlePattern}
-              onChange={this.onTabTitlePatternChange}
-              loading={this.state.applyLoading}
-              disabled={this.state.groupStrategy !== 2}
-              onSearch={this.onTabTitlePatternApply}
+          {this.state.groupStrategy === 1 && (
+            <Alert
+              message="Full domain name. Match 'www.google.com' in https://www.google.com/"
+              type="info"
             />
-          </Form.Item>
+          )}
+          {this.state.groupStrategy === 2 && (
+            <Alert
+              message="Second Level Domain. Match 'google' in https://www.google.com/"
+              type="info"
+            />
+          )}
+          {this.state.groupStrategy === 3 && (
+            <Form.Item label="Tab title contains">
+              <Input.Search
+                enterButton="Apply"
+                placeholder="Empty matches nothing"
+                value={this.state.tabTitlePattern}
+                onChange={this.onTabTitlePatternChange}
+                loading={this.state.applyLoading}
+                disabled={this.state.groupStrategy !== 3}
+                onSearch={this.onTabTitlePatternApply}
+              />
+            </Form.Item>
+          )}
         </Form>
       </div>
     );
