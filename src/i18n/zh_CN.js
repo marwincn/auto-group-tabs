b16310cf2ef7650dde2b22e3de4152df6896fc03
marwincn <qiangbo.xu@gmail.com>
2021-10-27 22:06:59 +0800

    [Feature]增加国际化，支持中文和英文

---
+++ src/i18n/zh_CN.js
@@ -0,0 +1,14 @@
+export default {
+  group_all_tabs: "一键分组所有Tabs",
+  enable_auto_group: "是否开启自动分组：",
+  min_number: "每个分组最小Tab数量：",
+  group_strategy: "分组策略：",
+  domain: "域名",
+  domain_tip: "从 https://www.google.com/ 中匹配 'www.google.com'",
+  sld: "二级域名",
+  sld_tip: "从 https://www.google.com/ 中匹配 'google'",
+  tab_title: "Tab标题",
+  tab_title_contains: "Tab标题包含：",
+  apply: "应用",
+  input_placeholder: "不填则不匹配",
+};
